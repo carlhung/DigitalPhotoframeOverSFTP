e7a5b0adf5ab5b5939dad56530fcbc630582143b
carlhung <carl.hung@yahoo.com>
2025-07-27 22:26:06 +0100

    updated error message

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 void showErrorOnSnackBar(BuildContext context, Object error) {
   ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(duration: const Duration(seconds: 5), content: Text('$error')),
+    SnackBar(
+      duration: const Duration(seconds: 5),
+      content: Text(error.toString()),
+    ),
   );
 }
