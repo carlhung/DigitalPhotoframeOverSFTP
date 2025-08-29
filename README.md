cac10d4787ecae627a5a6535270fcfe825a0f5cd
carlhung <carl.hung@yahoo.com>
2025-08-29 01:56:52 +0100

    added firebase. but it hasn't added google drive package yet.

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ I have some photos in the server. So, I created the app to show the photos over 
 I tested it on windows and android. It doesn't support Web. The rest, test yourself.
 
 ![alt text](loginScreen.jpeg "Log In Screen")
+
+
+Platform  Firebase App Id
+web       1:27319422735:web:312762bd4b59121d71787a
+android   1:27319422735:android:68d6bc0c39858e4371787a
+ios       1:27319422735:ios:8a0c58dd661b2ea671787a
+macos     1:27319422735:ios:8a0c58dd661b2ea671787a
+windows   1:27319422735:web:8d740e2cd94374f071787a
--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -9,7 +12,6 @@ android {
     ndkVersion = "27.0.12077973"
     namespace = "com.example.photoframe"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,7 +27,7 @@ android {
         applicationId = "com.example.photoframe"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
